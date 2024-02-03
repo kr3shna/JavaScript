4cc9929b360b783a45cb9abbdea7efd58cb04cf2
kr3shna <krishna2shende@gmail.com>
2024-02-03 10:50:49 +0530

    updating

--- 06_string.js
+++ 06_string.js
@@ -7,14 +7,18 @@
 //  const  lastname ="shende";
 // console.log(name + lastname);  // first way to add string
 
-// console.log(`MY name is ${name} and my surname is ${lastname}`)  // Second way to add string
+// console.log(`MY name is ${name} and my surname is ${lastname}`)  // Second way to add string/Modern way  it is known as string interpolation/template litrals/template string
 
 
-let mrp = 1299;
-let disprice = 799;
-console.log("The MRP of item is " + mrp);
-console.log("The Discounted price of item is " + disprice);
-let disper = ((mrp - disprice)/mrp) * 100;
-let roundPrice = Math.round(disper);
-console.log(`You Saved ${roundPrice}%`);
+//example
+// let mrp = 1299;
+// let disprice = 799;
+// console.log("The MRP of item is " + mrp);
+// console.log("The Discounted price of item is " + disprice);
+// let disper = ((mrp - disprice)/mrp) * 100;
+// let roundPrice = Math.round(disper);
+// console.log(`You Saved ${roundPrice}%`);
 
+
+
+//Methods in String
