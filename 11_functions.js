8b88d7a16560e4ce7c70bdfd20c32484f0992725
kr3shna <krishna2shende@gmail.com>
2024-02-08 18:58:28 +0530

    functions

--- 11_functions.js
+++ 11_functions.js
@@ -58,5 +58,37 @@ function loginUserMessage(username= "xyz"){
 //  console.log(loginUserMessage("klm"));
 
 
+
+function calculateCartPrice(...num1){                //here, ... is rest operator:-the rest operator puts the rest of some specific user-supplied values into a JavaScript array
+        return num1;                                 //though, num1 is returned but more values will also be return due to rest operator
+}
+
+// console.log(calculateCartPrice(100,200,300));
+
+function calculateCartPrice(val1,val2,...num1){
+          return num1;
+}
+// console.log(calculateCartPrice(100,200,300,400,500,700));       //output:- [300,400,500,700]      here, 100 and 200 are assign to val1 and val2 and rest all are assign in array
                                  
-                                                
+          
+const user = {
+    username: "abcd",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user);   // 1st way to pass the object
+
+handleObject({
+    username: "abcd",
+    price: 199
+})
+
+const myNewArray  = [200,300,400,500];
+function returnSecondValue(getArray){
+      return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
