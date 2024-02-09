191719c6c24a7a435bb19096de0cac4b022635e9
kr3shna <krishna2shende@gmail.com>
2024-02-09 14:14:31 +0530

    Iterations

--- 16_iteration.js
+++ 16_iteration.js
@@ -64,7 +64,119 @@
 // }
 
 // do while loop
-  let score = 1;
-do {
-    console.log(`Score is ${score}`);
-} while (score <= 10);
+ 
+// let score = 1;
+// do {
+//     console.log(`Score is ${score}`);
+// } while (score <= 10);
+
+
+// Array secific loops
+// for of loop
+
+
+// const arr = [1,2,3,4,5];
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+
+// const greetings = "hello duniya";
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+    
+// }
+
+
+//Maps
+
+// const map = new Map()
+// map.set('IN',"India");
+// map.set('USA',"United States of America");
+// map.set('Fr', "France");
+
+// console.log(map);
+
+// for of loop for map 
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+//  for of loop for object
+
+// const myObject = {
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+// }
+// for (const [key, value] of myObject){
+//     console.log(key, ':-', value);
+// }
+// Note:-  Objects are not iterable
+
+// for in loop
+
+
+// const myObject = {
+//     js: "javascript",
+//     cpp: "C++",
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+
+// const programing = ['js','rb','py','java','cpp']
+
+// for(const key in programing){
+//     console.log(programing[key]);
+// }
+
+
+
+// for each loop
+
+ const coding = ['js','ruby','python','cpp']
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+//using arrow function
+
+// coding.forEach((item) =>{
+//     console.log(item);
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item,index,arr);
+// })
+
+ const myCoding = [
+    {
+        languageName: 'javascript',
+        languageFileName: 'js'
+    },
+   {
+        languageName: 'java',
+        languageFileName:'java'
+   },
+   {
+       languageName: 'python',
+       languageFileName: 'py'
+   },
+]
+
+
+myCoding.forEach((item) =>{
+    console.log(item.languageName);      // accessing values from objects which are present inside array
+})
