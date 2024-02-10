49a95c9f7703745228be11ee9343525bc1977e7c
kr3shna <krishna2shende@gmail.com>
2024-02-10 17:13:21 +0530

    Conversion of Operators

--- 01_bacis/04_comparision.js
+++ 01_bacis/04_comparision.js
@@ -1,11 +1,11 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 <= 1);
-// console.log(2 != 1);
+// console.log(3 > 1);
+// console.log(3 >= 1);
+// console.log(3 < 1);
+// console.log(3 <= 1);
+// console.log(3 != 1);
 
 
-// console.log("2" > 0);
+// console.log("3" > 0);
 
 
 console.log(null > 0);
